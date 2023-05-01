21f69be0b4a5519ca345293e9668274ead44bb8b
Eric S <estanski@icloud.com>
2023-05-01 14:16:37 -0400

    Bugs fixed

---
+++ PlayFetchChallenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float cd = 0.5f;
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= cd)
+        {
+            timer = 0;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
